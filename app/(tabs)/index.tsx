ed22c3e42ad227e8af1af826c8f74d8c64f164c0
Yatik1 <yatiksrivastava1@gmail.com>
2025-01-17 13:51:59 +0530

    index page

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,12 +1,66 @@
-import { View, Text } from 'react-native'
+import { View, Text, StyleSheet, Pressable } from 'react-native'
 import React from 'react'
+import AntDesign from '@expo/vector-icons/AntDesign';
 
 const index = () => {
   return (
-    <View>
-      <Text>index</Text>
+    <View style={styles.container}>
+      <Text style={styles.heading}>Accomplish your goals!</Text>
+      <Text style={styles.text}>Organize it</Text>
+      <Pressable style={styles.button}>
+        <Text style={styles.buttonText}>Let's go</Text>
+        <AntDesign name="arrowright" size={24} color="black" />
+      </Pressable>
     </View>
-  )
-}
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: 'black',
+    padding:10,
+    gap:10
+  },
+  heading: {
+    color:"white",
+    fontSize:30,
+    fontWeight:"bold",
+  },
+  text: {
+    color:"gray",
+    fontSize:18,
+    fontStyle:"italic"
+  },
+  button: {
+    backgroundColor: 'white',
+    display:"flex",
+    flexDirection:"row",
+    paddingVertical: 15,
+    paddingHorizontal: 23,
+    width: 150,
+    marginTop: 50,
+    borderRadius: 8,
+    alignItems: 'center',
+    justifyContent:"center",
+    gap:10
+  },
+  buttonText: {
+    fontSize: 18,
+    color: 'black',
+  },
+  // absoluteView: {
+  //   position: 'absolute',
+  //   top: 0,
+  //   left: 0,
+  //   height: 150,
+  //   width: 150,
+  //   backgroundColor:'white',
+  //   borderRadius:100,
+  //   // overflow:'hidden'
+  // },
+});
 
 export default index
