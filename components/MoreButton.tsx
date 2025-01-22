3577a7846bce17c68659f9bc84baf8c6162b79e4
Yatik1 <yatiksrivastava1@gmail.com>
2025-01-22 15:21:20 +0530

    drop down button

--- components/MoreButton.tsx
+++ components/MoreButton.tsx
@@ -1,7 +1,7 @@
-import { StyleSheet, Text, TouchableOpacity, View } from 'react-native';
-// import Ionicons from '@expo/vector-icons/Ionicons';
-// import { Colors } from '@/constants/Colors';
-// import * as DropdownMenu from 'zeego/dropdown-menu';
+import { StyleSheet, TouchableOpacity } from 'react-native';
+import Ionicons from '@expo/vector-icons/Ionicons';
+import { Colors } from '@/constants/Colors';
+import * as DropdownMenu from 'zeego/dropdown-menu';
 
 
 
@@ -16,12 +16,59 @@ const MoreButton = ({ pageName }: MoreButtonProps) => {
   //   toast.success(`Page Link copied to your clipboard`);
   // };
 
-  // 
-  return(
-    <View>
-      <Text>More Button</Text>
-    </View>
-  )
+  return (
+    <DropdownMenu.Root>
+      <DropdownMenu.Trigger>
+        <TouchableOpacity style={styles.button} activeOpacity={0.6}>
+          <Ionicons name="ellipsis-horizontal-outline" size={30} color={Colors.primary} />
+        </TouchableOpacity>
+      </DropdownMenu.Trigger>
+
+      <DropdownMenu.Content>
+        <DropdownMenu.Item
+          key="link"
+          textValue={`Copy Link to ${pageName}`}
+          // onSelect={copyToClipboard}
+        >
+          <DropdownMenu.ItemTitle>Copy</DropdownMenu.ItemTitle>
+          <DropdownMenu.ItemIcon
+            ios={{
+              name: 'link',
+              pointSize: 24,
+            }}></DropdownMenu.ItemIcon>
+        </DropdownMenu.Item>
+
+        <DropdownMenu.Group>
+          <DropdownMenu.Item key="select">
+            <DropdownMenu.ItemTitle>Select Tasks</DropdownMenu.ItemTitle>
+            <DropdownMenu.ItemIcon
+              ios={{
+                name: 'square.stack',
+                pointSize: 24,
+              }}></DropdownMenu.ItemIcon>
+          </DropdownMenu.Item>
+
+          <DropdownMenu.Item key="view">
+            <DropdownMenu.ItemTitle>View</DropdownMenu.ItemTitle>
+            <DropdownMenu.ItemIcon
+              ios={{
+                name: 'slider.horizontal.3',
+                pointSize: 24,
+              }}></DropdownMenu.ItemIcon>
+          </DropdownMenu.Item>
+
+          <DropdownMenu.Item key="activity">
+            <DropdownMenu.ItemTitle>Activity Log</DropdownMenu.ItemTitle>
+            <DropdownMenu.ItemIcon
+              ios={{
+                name: 'chart.xyaxis.line',
+                pointSize: 24,
+              }}></DropdownMenu.ItemIcon>
+          </DropdownMenu.Item>
+        </DropdownMenu.Group>
+      </DropdownMenu.Content>
+    </DropdownMenu.Root>
+  );
 };
 
 export default MoreButton;
