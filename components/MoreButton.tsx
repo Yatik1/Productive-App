08cc7816fe66dd8842d068004974060d57ec2c7f
Yatik1 <yatiksrivastava1@gmail.com>
2025-01-22 15:12:01 +0530

    dialog component

--- components/MoreButton.tsx
+++ components/MoreButton.tsx
@@ -1,9 +1,9 @@
-import { StyleSheet, TouchableOpacity } from 'react-native';
-import Ionicons from '@expo/vector-icons/Ionicons';
-import { Colors } from '@/constants/Colors';
-import * as DropdownMenu from 'zeego/dropdown-menu';
-import { toast } from 'sonner-native';
-// import * as Clipboard from 'expo-clipboard';
+import { StyleSheet, Text, TouchableOpacity, View } from 'react-native';
+// import Ionicons from '@expo/vector-icons/Ionicons';
+// import { Colors } from '@/constants/Colors';
+// import * as DropdownMenu from 'zeego/dropdown-menu';
+
+
 
 
 type MoreButtonProps = {
@@ -11,64 +11,17 @@ type MoreButtonProps = {
 };
 
 const MoreButton = ({ pageName }: MoreButtonProps) => {
-  const copyToClipboard = async () => {
-    const path = `myapp://(authenticated)/(tabs)/${pageName.toLowerCase()}`;
-    // await Clipboard.setStringAsync(path);
-    toast.success(`Page Link copied to your clipboard`);
-  };
+  // const copyToClipboard = async () => {
+  //   const path = `todoapp://(authenticated)/(tabs)/${pageName.toLowerCase()}`;
+  //   toast.success(`Page Link copied to your clipboard`);
+  // };
 
-  return (
-    <DropdownMenu.Root>
-      <DropdownMenu.Trigger>
-        <TouchableOpacity style={styles.button} activeOpacity={0.6}>
-          <Ionicons name="ellipsis-horizontal-outline" size={30} color={Colors.primary} />
-        </TouchableOpacity>
-      </DropdownMenu.Trigger>
-
-      <DropdownMenu.Content>
-        <DropdownMenu.Item
-          key="link"
-          textValue={`Copy Link to ${pageName}`}
-          onSelect={copyToClipboard}>
-          <DropdownMenu.ItemTitle>Copy</DropdownMenu.ItemTitle>
-          <DropdownMenu.ItemIcon
-            ios={{
-              name: 'link',
-              pointSize: 24,
-            }}></DropdownMenu.ItemIcon>
-        </DropdownMenu.Item>
-
-        <DropdownMenu.Group>
-          <DropdownMenu.Item key="select">
-            <DropdownMenu.ItemTitle>Select Tasks</DropdownMenu.ItemTitle>
-            <DropdownMenu.ItemIcon
-              ios={{
-                name: 'square.stack',
-                pointSize: 24,
-              }}></DropdownMenu.ItemIcon>
-          </DropdownMenu.Item>
-
-          <DropdownMenu.Item key="view">
-            <DropdownMenu.ItemTitle>View</DropdownMenu.ItemTitle>
-            <DropdownMenu.ItemIcon
-              ios={{
-                name: 'slider.horizontal.3',
-                pointSize: 24,
-              }}></DropdownMenu.ItemIcon>
-          </DropdownMenu.Item>
-
-          <DropdownMenu.Item key="activity">
-            <DropdownMenu.ItemTitle>Activity Log</DropdownMenu.ItemTitle>
-            <DropdownMenu.ItemIcon
-              ios={{
-                name: 'chart.xyaxis.line',
-                pointSize: 24,
-              }}></DropdownMenu.ItemIcon>
-          </DropdownMenu.Item>
-        </DropdownMenu.Group>
-      </DropdownMenu.Content>
-    </DropdownMenu.Root>
-  );
+  // 
+  return(
+    <View>
+      <Text>More Button</Text>
+    </View>
+  )
 };
 
 export default MoreButton;
