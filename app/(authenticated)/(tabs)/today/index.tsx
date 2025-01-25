841c10c2005a461b52bf8929f2d36ffa335a36a0
Yatik1 <yatiksrivastava1@gmail.com>
2025-01-25 17:17:59 +0530

    Implement SectionList for Today screen with task data

--- app/(authenticated)/(tabs)/today/index.tsx
+++ app/(authenticated)/(tabs)/today/index.tsx
@@ -1,11 +1,26 @@
-import { View, Text, StyleSheet } from 'react-native'
-import React from 'react'
+import { View, Text, StyleSheet, SectionList } from 'react-native'
+import React, { useEffect, useState } from 'react'
 import Fab from '@/components/Fab'
+import { useAllData } from '@/hooks/useAllData'
+import TaskRow from '@/components/TaskRow'
+import { Section } from '@/types/types'
+
 
 const index = () => {
+
+  const data : Section[] = useAllData()
+  console.log(data)
+  
+
+
   return (
     <View style={styles.container}>
-      <Text>Today</Text>
+      <SectionList 
+        contentInsetAdjustmentBehavior='automatic'
+        sections={data}
+        renderItem={({item}) => <TaskRow task={item} />}
+        renderSectionHeader={({section}) => <Text>{section.title}</Text>}
+      />
       <Fab />
     </View>
   )
