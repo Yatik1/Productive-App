aa09a7a3a86fc4f26df87712faa4c41001830475
Yatik1 <yatiksrivastava1@gmail.com>
2025-01-25 17:18:23 +0530

    Add Todo and Section TypeScript interfaces

---
+++ types/types.ts
@@ -0,0 +1,12 @@
+export interface Todo {
+    id:number, 
+    title:string,
+    description:string,
+    created_at:string, 
+    isCompleted:boolean
+}
+
+export interface Section {
+    title:string, 
+    data: Todo[]
+  }
