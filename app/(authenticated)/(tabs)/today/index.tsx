ad37eefb084118ad2cefef123ab0064d0da358d9
Yatik1 <yatiksrivastava1@gmail.com>
2025-01-29 14:07:57 +0530

    Refactor Today screen to fetch and format tasks dynamically

--- app/(authenticated)/(tabs)/today/index.tsx
+++ app/(authenticated)/(tabs)/today/index.tsx
@@ -1,25 +1,61 @@
 import { View, Text, StyleSheet, SectionList } from 'react-native'
 import React, { useEffect, useState } from 'react'
-import Fab from '@/components/Fab'
-import { useAllData } from '@/hooks/useAllData'
+import { format } from 'date-fns';
+import { Section, Todo } from '@/types/types';
 import TaskRow from '@/components/TaskRow'
-import { Section } from '@/types/types'
 
+import { Colors } from '@/constants/Colors'
+import Fab from '@/components/Fab';
+
+  const uri = "http://127.0.0.1:8000/api/todo/all/"
 
 const index = () => {
 
-  const data : Section[] = useAllData()
-  console.log(data)
+    const [sectionListData,setSectionListData] = useState<Section[]>([])
+    
+    const fetchData = async () => {
+
+      try {
+        const response = await fetch(uri)
+        const jsonResponse : Todo[] = await response.json()
+        const formattedData = jsonResponse?.filter((todo : {isCompleted:boolean}) => todo.isCompleted === false)
+        
+        const dataByDate = formattedData?.reduce((acc:{[key:string] : Todo[]},task:Todo) => {
+            const day = format(new Date(task.created_at || new Date()) , 'd MMM . eee')
+            if(!acc[day]) {
+              acc[day] = []
+            }
+            acc[day].push(task)
+            return acc
+        } , {})
+
+        const listData : Section[] = Object.entries(dataByDate || {}).map(([day,tasks]) => ({
+          title:day,
+          data:tasks
+        }))
+
+        setSectionListData(listData)
+        
+      } catch (error) {
+        console.log(error)
+      }
+
+    }
+
+    useEffect(() => {
+        fetchData()
+    }, [])
   
 
 
   return (
     <View style={styles.container}>
       <SectionList 
+        showsVerticalScrollIndicator={false}
         contentInsetAdjustmentBehavior='automatic'
-        sections={data}
-        renderItem={({item}) => <TaskRow task={item} />}
-        renderSectionHeader={({section}) => <Text>{section.title}</Text>}
+        sections={sectionListData}
+        renderItem={({item}) => <TaskRow task={item} fetchData={fetchData} />}
+        renderSectionHeader={({section}) => <Text style={styles.header}>{section.title}</Text>}
       />
       <Fab />
     </View>
@@ -30,6 +66,14 @@ const styles = StyleSheet.create({
   container: {
     flex:1,
     marginBottom:82 
+  },
+  header:{
+    fontSize:16,
+    backgroundColor:'#fff',
+    fontWeight:'bold',
+    padding:14,
+    borderBottomWidth: StyleSheet.hairlineWidth,
+    borderBottomColor: Colors.lightBorder,
   }
 })
 
